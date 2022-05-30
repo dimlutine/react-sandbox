36f25d9e35e76159e50c0a317f19bce213e98b3e
Jeremy Rowley <dimlutine@gmail.com>
2022-05-30 15:10:03 -0400

    add UseRef example 3 fix memory leak

---
+++ react-sandbox/src/components/UseRefExample3.jsx
@@ -0,0 +1,12 @@
+import Todo from "./Todo"
+import { useState } from 'react';
+
+function UseRefExample3() {
+    const [showTodo, setShowTodo] = useState(true)
+  return (
+    <div>{showTodo && <Todo />}
+    <button onClick={()=>setShowTodo(!showTodo)} className="btn btn-primary">Toggle Todo</button>
+    </div>
+  )
+}
+export default UseRefExample3
